9f2e096e3f429c752d4c60b526799c027985a85d
Kuetone <lujohnathon@gmail.com>
2023-03-16 16:46:12 -0700

    final product of #1 no print statements just function

--- hw4/palin.py
+++ hw4/palin.py
@@ -1,26 +1,8 @@
 def palindrone(lst): 
-#     lst = [1,2,3,4]
           palin = True
           length = len(lst)
           for i in range(int(len(lst)/2)):
-                    print(i, " & ", length-i-1)
                     if (lst[i] != lst[length-i-1]):
                               palin = False
-                    # print(lst[i])
-          
-                    
-          
-#     for i in (range(len(lst)))/2:
-#         if (lst[i] != lst[length-i]):
-#             palin = False
- 
-          return palin
-    
-list1 = [1, 2, "Espresso", "Madeline", 2, 1]
-list2 = ['a', True, False, False, True, 'a']
-list3 = [3]
 
-print(palindrone(list1))
-print(palindrone(list2))
-print(palindrone(list3))
 
