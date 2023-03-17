9eb8a3bcb7f47985078854fcc1e3e75fb4ea423d
Kuetone <lujohnathon@gmail.com>
2023-03-16 17:13:27 -0700

    #2 no print no tests

--- hw4/merge.py
+++ hw4/merge.py
@@ -13,17 +13,4 @@ def merge(list1, list2):
           while(list2Count < len(list2)):
                     listFinal.append(list2[list2Count])
                     list2Count += 1
-          print(listFinal)
           return listFinal
-
-list1= [1, 3, 5, 7]
-list2= [2, 4, 6]
-merge(list1, list2)
-                             
-list1 =  [1, 2, 3, 5]
-list2 = [1, 2, 4, 5, 6]
-merge(list1, list2)
-
-list1 =  [1, 5, 9]
-list2 = []
-merge(list1, list2)
