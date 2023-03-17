6d042517e23c8ef0c5e07ed1635a52ea55bbec06
Kuetone <lujohnathon@gmail.com>
2023-03-16 21:18:11 -0700

    #3 Final copy

--- hw4/sort.py
+++ hw4/sort.py
@@ -12,17 +12,6 @@ def sort_dictionary(dict):
                               if i[1][1] < minItem[1][1]:
                                         minItem = i
                     dict2.append(tuple([minItem[0], minItem[1][0]]))
-                    # print(dict)
                     dict.pop(minItem[0])
                               
-          # dict2.update({minItem[0], minItem[1][0]})
           return dict2
-          # while (len(dict2) < len(dict)):
-dict =  {
-          'Tom' : (5464512, 24) , 
-          'Sara' : (5446987, 32) , 
-          'Mary' : (1557896, 20)
-          }
-# print(dict)
-
-print(sort_dictionary(dict))
