a2a118c8d6e1613d1f9705ed4e99307e1411b0b1
Kuetone <lujohnathon@gmail.com>
2023-03-18 20:37:01 -0700

    transfer to desktop

---
+++ cacti.py
@@ -0,0 +1,57 @@
+def cacti_number(arr2d):
+          def wrapper():
+                    ROWS = len(arr2d)
+                    COLLS = len(arr2d[0])
+                    moreCactiCount = 0
+                    for row in range(len(arr2d)):
+                              for element in range(len(arr2d[row])):
+                                        if (row == 0): #first row
+                                                  if (element == 0 and arr2d[row][element] == 0): #leftmost element = 0
+                                                            if (arr2d[row + 1][element] == 0 and arr2d[row][element + 1] == 0): #check surrounding cases (below, right)
+                                                                      arr2d[row][element] = 1
+                                                                      moreCactiCount += 1
+                                                  if (element > 0 and element < COLLS - 1 and arr2d[row][element] == 0): #middle element = 0
+                                                            if (arr2d[row + 1][element] == 0 and arr2d[row][element + 1] == 0 and arr2d[row][element - 1] == 0): #check surrounding cases (below, right, left)
+                                                                      arr2d[row][element] = 1
+                                                                      moreCactiCount += 1
+                                                  if (element == COLLS - 1 and arr2d[row][element] == 0): #rightmost element = 0
+                                                            if (arr2d[row + 1][element] == 0 and arr2d[row][element - 1] == 0): #check surrounding cases (below, left)
+                                                                      arr2d[row][element] = 1
+                                                                      moreCactiCount += 1
+                                                                      
+                                        if (row > 0 and row < ROWS - 1): #middle rows 
+                                                  if (element == 0 and arr2d[row][element] == 0): #leftmost element = 0
+                                                            if (arr2d[row + 1][element] == 0 and arr2d[row][element + 1] == 0 and arr2d[row - 1][element] == 0): #check surrounding cases (below, right, top)
+                                                                      arr2d[row][element] = 1
+                                                                      moreCactiCount += 1
+                                                  if (element > 0 and element < COLLS - 1 and arr2d[row][element] == 0): #middle element = 0
+                                                            if (arr2d[row + 1][element] == 0 and arr2d[row][element + 1] == 0 and arr2d[row][element - 1] == 0 and arr2d[row - 1][element] == 0): #check surrounding cases (below, right, left, top)
+                                                                      arr2d[row][element] = 1
+                                                                      moreCactiCount += 1
+                                                  if (element == COLLS - 1 and arr2d[row][element] == 0): #rightmost element = 0
+                                                            if (arr2d[row + 1][element] == 0 and arr2d[row][element - 1] == 0 and arr2d[row - 1][element] == 0): #check surrounding cases (below, left, top)
+                                                                      arr2d[row][element] = 1
+                                                                      moreCactiCount += 1
+                                        if (row == ROWS - 1): #last row
+                                                  if (element == 0 and arr2d[row][element] == 0): #leftmost element = 0
+                                                            if (arr2d[row - 1][element] == 0 and arr2d[row][element + 1] == 0): #check surrounding cases (top, right)
+                                                                      arr2d[row][element] = 1
+                                                                      moreCactiCount += 1
+                                                  if (element > 0 and element < COLLS - 1 and arr2d[row][element] == 0): #middle element = 0
+                                                            if (arr2d[row - 1][element] == 0 and arr2d[row][element + 1] == 0 and arr2d[row][element - 1] == 0): #check surrounding cases (top, right, left)
+                                                                      arr2d[row][element] = 1
+                                                                      moreCactiCount += 1
+                                                  if (element == COLLS - 1 and arr2d[row][element] == 0): #rightmost element = 0
+                                                            if (arr2d[row - 1][element] == 0 and arr2d[row][element - 1] == 0): #check surrounding cases (top, left)
+                                                                      arr2d[row][element] = 1
+                                                                      moreCactiCount += 1
+                    return moreCactiCount
+          return wrapper                                                                      
+                                                                      
+def main():
+          plot = [ [1, 0, 1, 0, 1],
+                  [0, 0, 0, 0, 0],
+                  [1, 0, 0, 0, 0] ]
+          print(cacti_number(plot))
+
+main()
